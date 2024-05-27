6ee25747895a9897e4b955be73360809bbd8e4de
Paola Gomes <paola@frengee.com>
2024-05-27 00:01:00 -0300

    feat: style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,24 @@ const config: Config = {
       fontFamily: {
         catamaran: ["var(--font-catamaran)"],
       },
+      colors: {
+        yellow: {
+          light: "var(--yellow-light)",
+          default: "var(--yellow)",
+          dark: "var(--yellow-dark)",
+        },
+        gray: {
+          100: "var(--gray-100)",
+          200: "var(--gray-200)",
+          300: "var(--gray-300)",
+          400: "var(--gray-400)",
+          500: "var(--gray-500)",
+          600: "var(--gray-600)",
+          700: "var(--gray-700)",
+          800: "var(--gray-800)",
+          900: "var(--gray-900)",
+        },
+      },
     },
   },
   plugins: [],
